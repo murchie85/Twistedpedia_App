da76cd2786e690ee201421bab0d80ed0b3648f9d
Adam <murchie85@gmail.com>
2017-02-17 23:08:42 +0000

    update2

--- app/src/main/java/com/example/android/twistedpedia/Twisted.java
+++ app/src/main/java/com/example/android/twistedpedia/Twisted.java
@@ -3,6 +3,38 @@ package com.example.android.twistedpedia;
 /**
  * Created by adammcmurchie on 17/02/2017.
  */
-
+//joke class
 public class Twisted {
+
+    //joke Date
+    private String mDate;
+    //joke Poster
+    private String mUser;
+    // jokescore
+    private String mScore;
+    //joke
+    private String mJoke;
+
+
+    // CONSTRUCT A NEW JOKE
+
+    public Twisted(String date, String user, String score, String joke){
+
+        mDate = date;
+        mUser = user;
+        mScore = score;
+        mJoke = joke;
+    }
+
+    // RETURNS constructed variables
+
+    public  String  getmDate(){return mDate;}
+    public  String getmUser(){return  mUser;}
+    public  String getmScore(){return  mScore;}
+    public  String getmJoke(){return mJoke;}
+
 }
+
+
+
+
