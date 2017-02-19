0af91b02bd8a0eaaca24510942b04642915a72cc
Adam <murchie85@gmail.com>
2017-02-19 16:39:34 +0000

    newer

--- app/src/main/java/com/example/android/twistedpedia/TwistedToday.java
+++ app/src/main/java/com/example/android/twistedpedia/TwistedToday.java
@@ -1,13 +1,16 @@
 package com.example.android.twistedpedia;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class TwistedActivity extends AppCompatActivity {
+/**
+ * Created by adammcmurchie on 19/02/2017.
+ */
 
+public class TwistedToday extends AppCompatActivity{
     public static final String LOG_TAG = TwistedActivity.class.getName();
 
 
@@ -18,18 +21,18 @@ public class TwistedActivity extends AppCompatActivity {
 
 
         // creates in a list of TwistedObjects
-        // this will eventually just be the best jokes of this month.
+        // this will eventually just be the best jokes today.
         ArrayList<Twisted> jokes = new ArrayList<>();
-        jokes.add(new Twisted("21.01.2017","Murchie85","120","To all you beautiful girls out there Happy Valentines day... To all you fat birds chins up, its Pancake day next week...\n"));
-        jokes.add(new Twisted("24.01.2017","DantheMan","130","Apparently, 1 in 10 people live next door to a pedophile. \n" +
+        jokes.add(new Twisted("21.01.2017","Murchie85","12","To all you beautiful girls out there Happy Valentines day... To all you fat birds chins up, its Pancake day next week...\n"));
+        jokes.add(new Twisted("24.01.2017","DantheMan","30","Apparently, 1 in 10 people live next door to a pedophile. \n" +
                 "Not me though, I live next door to a really sexy 10 year-old girl.\n"));
-        jokes.add(new Twisted("24.01.2017","Lyds","110","Apparently, 1 in 10 people live next door to a pedophile. \n" +
+        jokes.add(new Twisted("24.01.2017","Lyds","10","Apparently, 1 in 10 people live next door to a pedophile. \n" +
                 "Not me though, I live next door to a really sexy 10 year-old girl.\n"));
-        jokes.add(new Twisted("24.01.2017","Allan","150","Apparently, 1 in 10 people live next door to a pedophile. \n" +
+        jokes.add(new Twisted("24.01.2017","Allan","50","Apparently, 1 in 10 people live next door to a pedophile. \n" +
                 "Not me though, I live next door to a really sexy 10 year-old girl.\n"));
-        jokes.add(new Twisted("29.01.2017","Andy","150","Apparently, 1 in 10 people live next door to a pedophile. \n" +
+        jokes.add(new Twisted("29.01.2017","Andy","20","Apparently, 1 in 10 people live next door to a pedophile. \n" +
                 "Not me though, I live next door to a really sexy 10 year-old girl.\n"));
-        jokes.add(new Twisted("01.02.2017","John","150","Apparently, 1 in 10 people live next door to a pedophile. \n" +
+        jokes.add(new Twisted("01.02.2017","John","60","Apparently, 1 in 10 people live next door to a pedophile. \n" +
                 "Not me though, I live next door to a really sexy 10 year-old girl.\n"));
 
 
@@ -44,9 +47,5 @@ public class TwistedActivity extends AppCompatActivity {
         twistedListView.setAdapter(adapter);
 
     }
-
-
-
-
 
 }
