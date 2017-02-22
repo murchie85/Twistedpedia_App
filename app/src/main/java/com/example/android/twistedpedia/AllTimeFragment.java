1b14e28c3da3ee28d3875d666f5794e75c08fc71
Adam <murchie85@gmail.com>
2017-02-22 22:29:23 +0000

    refact

--- app/src/main/java/com/example/android/twistedpedia/AllTimeFragment.java
+++ app/src/main/java/com/example/android/twistedpedia/AllTimeFragment.java
@@ -1,12 +1,16 @@
 package com.example.android.twistedpedia;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,13 +22,59 @@ public class AllTimeFragment extends Fragment {
         // Required empty public constructor
     }
 
+    public static final String LOG_TAG = TwistedActivity.class.getName();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
+
+
+        // creates in a list of TwistedObjects
+        // this will eventually just be the best jokes of all time
+        ArrayList<Twisted> jokes = new ArrayList<>();
+        jokes.add(new Twisted("21.01.2017","Murchie85","0","To all you beautiful girls out there Happy Valentines day... To all you fat birds chins up, its Pancake day next week...\n"));
+        jokes.add(new Twisted("24.01.2017","DantheMan","30","Apparently, 1 in 10 people live next door to a pedophile. \n" +
+                "Not me though, I live next door to a really sexy 10 year-old girl.\n"));
+        jokes.add(new Twisted("24.01.2017","Lyds","10","Apparently, 1 in 10 people live next door to a pedophile. \n" +
+                "Not me though, I live next door to a really sexy 10 year-old girl.\n"));
+        jokes.add(new Twisted("24.01.2017","Allan","50","Apparently, 1 in 10 people live next door to a pedophile. \n" +
+                "Not me though, I live next door to a really sexy 10 year-old girl.\n"));
+        jokes.add(new Twisted("29.01.2017","Andy","50","Apparently, 1 in 10 people live next door to a pedophile. \n" +
+                "Not me though, I live next door to a really sexy 10 year-old girl.\n"));
+        jokes.add(new Twisted("01.02.2017","John","50","Apparently, 1 in 10 people live next door to a pedophile. \n" +
+                "Not me though, I live next door to a really sexy 10 year-old girl.\n"));
+
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView twistedListView = (ListView) rootView.findViewById(R.id.list);
+
+        // Create a new {adaptor that takes a list of earthquakes as input
+        TwistedAdaptor adapter = new TwistedAdaptor(this, jokes);
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        twistedListView.setAdapter(adapter);
+
+
+
+        //-----------------------ON CLICK LISTENER FAMILY ----------------------------------//
+
+        // Find the View that shows the family category
+        TextView family = (TextView) findViewById(R.id.main);
+
+        // Set a click listener on that View
+        family.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the family category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link FamilyActivity}
+                Intent alltimeIntent = new Intent(TwistedAllTime.this, TwistedActivity.class);
+
+                // Start the new activity
+                startActivity(alltimeIntent);
+            }
+        });
     }
 
 }
