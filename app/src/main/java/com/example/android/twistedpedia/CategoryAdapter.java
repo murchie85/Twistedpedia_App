a29ee48d28f6389f39e71c04fd8e8f534cca2f56
Adam <murchie85@gmail.com>
2017-03-05 19:50:39 +0000

    fragements

---
+++ app/src/main/java/com/example/android/twistedpedia/CategoryAdapter.java
@@ -0,0 +1,24 @@
+package com.example.android.twistedpedia;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Adam McMurchie on
+ */
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    public CategoryAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
