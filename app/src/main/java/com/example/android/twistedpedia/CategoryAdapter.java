ea4e50eb872fbc0fcfcd71e659413a939ce91ebb
Adam <murchie85@gmail.com>
2017-03-08 22:58:59 +0000

    stuff

--- app/src/main/java/com/example/android/twistedpedia/CategoryAdapter.java
+++ app/src/main/java/com/example/android/twistedpedia/CategoryAdapter.java
@@ -8,17 +8,33 @@ import android.support.v4.app.FragmentPagerAdapter;
  */
 public class CategoryAdapter extends FragmentPagerAdapter {
 
+    /**
+     * Create a new {@link CategoryAdapter} object.
+     *
+     * @param fm is the fragment manager that will keep each fragment's state in the adapter
+     *           across swipes.
+     */
     public CategoryAdapter(FragmentManager fm) {
         super(fm);
     }
 
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
     @Override
     public Fragment getItem(int position) {
-        return null;
+        if (position == 0) {
+            return new AllTimeFragment();
+        } else {
+            return new TodayFragment();
+        }
     }
 
+    /**
+     * Return the total number of pages.
+     */
     @Override
     public int getCount() {
-        return 0;
+        return 1;
     }
 }
